df4bb9259126f9a50c3f790ed3d6d0008e3f9828
Jelon <86931022+Jelon1211@users.noreply.github.com>
2024-05-23 00:01:34 +0200

    open google maps

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -5,13 +5,13 @@ import {
   TouchableOpacity,
   StatusBar,
   Image,
+  Linking,
 } from "react-native";
 import React, { useState } from "react";
 import MapView from "react-native-maps";
 import { ICurrentLocation, IRegion, MapProps } from "@/types/map";
 import LocationMarker from "./LocationMarker";
 import mockMarkers from "@/constants/mockMarkers";
-import CancelButton from "../CancelButton";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 
@@ -31,6 +31,13 @@ const Map: React.FC<MapProps> = ({ currentLocation }) => {
   const handleMarkerPress = (marker: ICurrentLocation) => {
     setSelectedMarker(marker);
     setModalVisible(true);
+  };
+
+  const openGoogleMaps = () => {
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${currentLocation.latitude},${currentLocation.longitude}`;
+    Linking.openURL(url).catch((err) =>
+      console.error("An error occurred", err)
+    );
   };
 
   return (
@@ -76,17 +83,29 @@ const Map: React.FC<MapProps> = ({ currentLocation }) => {
           onRequestClose={() => setModalVisible(false)}
         >
           <View className="flex-1 justify-center items-center bg-black/25">
-            <View className="w-10/12 p-3 rounded-lg bg-white">
+            <View className="w-10/12 px-3 pt-3 rounded-lg bg-white border-solid border-2">
+              <View>
+                <TouchableOpacity
+                  className="absolute -right-8 -top-8 z-50 w-16 h-16"
+                  onPress={() => setModalVisible(false)}
+                >
+                  <Image
+                    source={icons.cancel}
+                    className="w-5 h-5 absolute right-6 top-6"
+                    resizeMode="contain"
+                  />
+                </TouchableOpacity>
+              </View>
               <Text className="text-2xl font-bold text-center">
                 Znajdź żółtego psa lub diabła rogatego
               </Text>
               <Image
                 source={images.thumbnail}
-                className="w-full"
+                className="w-full max-w-72 max-h-52"
                 resizeMode="contain"
               />
-              <View className="flex-row items-end">
-                <Text className="mt-4 text-base">
+              <View className="w-100">
+                <Text className="text-base">
                   {/* 40 words max */}
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Dolore laudantium possimus cum pariatur, cumque fuga enim nam
@@ -94,19 +113,21 @@ const Map: React.FC<MapProps> = ({ currentLocation }) => {
                   dolores similique ratione. Laboriosam facere eligendi dolorum
                   hic voluptate unde eveniet fugit odit tempore recusandae!
                 </Text>
-                <View className="gap-7 border-solid border-2">
+                <View className="flex flex-row gap-7 pt-3 items-center justify-end">
                   <Image
                     source={icons.seeMore}
                     className="w-6 h-6"
                     tintColor="#FF9C01"
                     resizeMode="contain"
                   />
-                  <Image
-                    source={icons.map}
-                    className="w-8 h-8"
-                    tintColor="#FF9C01"
-                    resizeMode="contain"
-                  />
+                  <TouchableOpacity onPress={openGoogleMaps}>
+                    <Image
+                      source={icons.map}
+                      className="w-8 h-8"
+                      tintColor="#FF9C01"
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
                   <Image
                     source={icons.menu}
                     className="w-8 h-8"
